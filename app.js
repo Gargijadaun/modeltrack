6547a7b7211509c5395e76194a250043d3b7eca5
gargi <131345129+GargiJad@users.noreply.github.com>
2023-12-18 18:00:56 +0530

    dgf

--- app.js
+++ app.js
@@ -1,41 +1,131 @@
-AFRAME.registerComponent('ar-trigger', {
-    init: function () {
-        const scene = this.el.sceneEl.object3D;
+// declare global variable
+let video = null; // video element
+let detector = null; // detector object
+let detections = []; // store detection result
+let videoVisibility = true;
+let detecting = false;
 
-        // Create the tracked model (serving as a marker)
-        const trackedModel = document.createElement('a-entity');
-        trackedModel.setAttribute('gltf-model', 'scene.gltf');
-        trackedModel.setAttribute('scale', '0.1 0.1 0.1');
-        trackedModel.setAttribute('rotation', '-90 0 0');
-        trackedModel.setAttribute('position', '0 0 0');
+// global HTML element
+const toggleVideoEl = document.getElementById('toggleVideoEl');
+const toggleDetectingEl = document.getElementById('toggleDetectingEl');
 
-        // Create the triggering model
-        const triggerModel = document.createElement('a-entity');
-        triggerModel.setAttribute('gltf-model', 'scene.gltf');
-        triggerModel.setAttribute('scale', '0.1 0.1 0.1');
-        triggerModel.setAttribute('rotation', '-90 0 0');
-        triggerModel.setAttribute('position', '0 0 0');
-        triggerModel.setAttribute('visible', 'false'); // Initially hidden
+// set cursor to wait until video elment is loaded
+document.body.style.cursor = 'wait';
 
-        // Create the marker pattern
-        const marker = document.createElement('a-marker-pattern');
-        marker.setAttribute('preset', 'custom');
-        marker.appendChild(trackedModel);
+// The preload() function if existed is called before the setup() function
+function preload() {
+  // create detector object from "cocossd" model
+  detector = ml5.objectDetector('cocossd');
+  console.log('detector object is loaded');
+}
 
-        // Attach the triggering model to the tracked model
-        trackedModel.appendChild(triggerModel);
+// The setup() function is called once when the program starts.
+function setup() {
+  // create canvas element with 640 width and 480 height in pixel
+  createCanvas(640, 480);
+  // Creates a new HTML5 <video> element that contains the audio/video feed from a webcam.
+  // The element is separate from the canvas and is displayed by default.
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  console.log('video element is created');
+  video.elt.addEventListener('loadeddata', function() {
+    // set cursor back to default
+    if (video.elt.readyState >= 2) {
+      document.body.style.cursor = 'default';
+      console.log('video element is ready! Click "Start Detecting" to see the magic!');
+    }
+  });
+}
 
-        // Add the marker to the scene
-        scene.add(marker);
+// the draw() function continuously executes until the noLoop() function is called
+function draw() {
+  if (!video || !detecting) return;
+  // draw video frame to canvas and place it at the top-left corner
+  image(video, 0, 0);
+  // draw all detected objects to the canvas
+  for (let i = 0; i < detections.length; i++) {
+    drawResult(detections[i]);
+  }
+}
 
-        // Event listener for when the marker is found
-        trackedModel.addEventListener('markerFound', () => {
-            triggerModel.setAttribute('visible', 'true'); // Show the triggering model
-        });
+/*
+Exaple of an detect object
+{
+    "label": "person",
+    "confidence": 0.8013999462127686,
+    "x": 7.126655578613281,
+    "y": 148.3782720565796,
+    "width": 617.7880859375,
+    "height": 331.60210132598877,
+}
+*/
+function drawResult(object) {
+  drawBoundingBox(object);
+  drawLabel(object);
+}
 
-        // Event listener for when the marker is lost
-        trackedModel.addEventListener('markerLost', () => {
-            triggerModel.setAttribute('visible', 'false'); // Hide the triggering model
-        });
-    },
-});
+// draw bounding box around the detected object
+function drawBoundingBox(object) {
+  // Sets the color used to draw lines.
+  stroke('green');
+  // width of the stroke
+  strokeWeight(4);
+  // Disables filling geometry
+  noFill();
+  // draw an rectangle
+  // x and y are the coordinates of upper-left corner, followed by width and height
+  rect(object.x, object.y, object.width, object.height);
+}
+
+// draw label of the detected object (inside the box)
+function drawLabel(object) {
+  // Disables drawing the stroke
+  noStroke();
+  // sets the color used to fill shapes
+  fill('white');
+  // set font size
+  textSize(24);
+  // draw string to canvas
+  text(object.label, object.x + 10, object.y + 24);
+}
+
+// callback function. it is called when object is detected
+function onDetected(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  detections = results;
+  // keep detecting object
+  if (detecting) {
+    detect(); 
+  }
+}
+
+function detect() {
+  // instruct "detector" object to start detect object from video element
+  // and "onDetected" function is called when object is detected
+  detector.detect(video, onDetected);
+}
+
+function toggleVideo() {
+  if (!video) return;
+  if (videoVisibility) {
+    video.hide();
+    toggleVideoEl.innerText = 'Show Video';
+  } else {
+    video.show();
+    toggleVideoEl.innerText = 'Hide Video';
+  }
+  videoVisibility = !videoVisibility;
+}
+
+function toggleDetecting() {
+  if (!video || !detector) return;
+  if (!detecting) {
+    detect();
+    toggleDetectingEl.innerText = 'Stop Detecting';
+  } else {
+    toggleDetectingEl.innerText = 'Start Detecting';
+  }
+  detecting = !detecting;
+}
